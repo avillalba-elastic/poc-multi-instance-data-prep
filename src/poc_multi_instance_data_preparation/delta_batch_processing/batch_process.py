a4db32d55815483e8c6cde9ce082a90042fd8751
Ana Villalba <ana.villalba@elastic.co>
2025-07-29 17:27:39 +0200

    Modify batch writing to delta lake

--- src/poc_multi_instance_data_preparation/delta_batch_processing/batch_process.py
+++ src/poc_multi_instance_data_preparation/delta_batch_processing/batch_process.py
@@ -1,32 +1,49 @@
 import pyarrow as pa
-import pyarrow.compute as pc
 from deltalake import DeltaTable, write_deltalake
 from loguru import logger
 
 
-def process(input_file: str, batch_size: int) -> pa.RecordBatch:
+def write_chunk_to_delta_table(chunk: pa.RecordBatch, index: int, output_path: str) -> None:
+    """Writes a chunk to the output delta lake table.
+
+    Args:
+        chunk (pa.RecordBatch): The chunk to write
+        index (int): The position of the chunk, to control `append` or `overwrite` mode
+        output_path (str): The output delta table path
+    """
+
+    mode = "overwrite" if index == 0 else "append"
+
+    write_deltalake(table_or_uri=output_path, data=chunk, mode=mode, schema_mode="overwrite")
+
+
+def process(input_path: str, output_path: str, batch_size: int) -> pa.RecordBatch:
     """Process Delta table in chunks.
 
     Args:
-        input_file (str): Path/to the delta table
+        input_path (str): Path/to the delta table
+        output_path (str): Path/to the output delta table
         batch_size (int): The size of the chunks
 
     Yields:
         pa.RecordBatch: The processed chunk
     """
 
-    dt = DeltaTable(table_uri=input_file, storage_options={"timeout": "3600s"})
+    dt = DeltaTable(table_uri=input_path, storage_options={"timeout": "3600s"})
 
     pa_dataset = dt.to_pyarrow_dataset()
 
     for i, chunk in enumerate(pa_dataset.to_batches(batch_size=batch_size)):
-        logger.info(f"Processing chunk {i}")
+        logger.info(f"Processing chunk {i}...")
 
         # Simulate a dummy processing
-        means = pc.list_mean(chunk.column("vector"))
-        processed_chunk = chunk.append_column("avg_embeddings", means)
+        chunk_df = chunk.to_pandas()
+        chunk_df["avg_embeddings"] = chunk_df.apply(lambda row: row["vector"].mean(), axis=1)
+        processed_chunk = pa.RecordBatch.from_pandas(chunk_df)
 
-        yield processed_chunk
+        write_chunk_to_delta_table(
+            processed_chunk, i, output_path
+        )  # commits new version for every chunk, but for this PoC is OK
 
 
 def main() -> None:  # noqa: D103
@@ -37,21 +54,7 @@ def main() -> None:  # noqa: D103
 
     output_path = "s3://mvp-mlops-platform/poc-multi-instance-data-prep-shards-batch-writes-delta/"
 
-    schema = pa.schema(
-        [
-            ("doc_id", pa.string()),
-            ("vector", pa.list_(pa.float32(), 1024)),
-        ]
-    )
-
-    reader = pa.RecordBatchReader.from_batches(schema, process(input_path, batch_size=batch_size))
-
-    write_deltalake(
-        output_path,
-        reader,
-        mode="overwrite",
-        configuration={"delta.targetFileSize": str(1000 * 1024 * 1024)},
-    )
+    process(input_path, output_path, batch_size)
 
 
 if __name__ == "__main__":
