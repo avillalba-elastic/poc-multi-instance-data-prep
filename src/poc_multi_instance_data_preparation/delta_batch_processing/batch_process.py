61e48e99a594848a151131e353629d1a73f6f7a3
Ana Villalba <ana.villalba@elastic.co>
2025-07-30 10:49:12 +0200

    Unify processing library and dummy processing operation

--- src/poc_multi_instance_data_preparation/delta_batch_processing/batch_process.py
+++ src/poc_multi_instance_data_preparation/delta_batch_processing/batch_process.py
@@ -1,20 +1,22 @@
+import time
+
 import pyarrow as pa
 from deltalake import DeltaTable, write_deltalake
 from loguru import logger
 
 
-def write_chunk_to_delta_table(chunk: pa.RecordBatch, index: int, output_path: str) -> None:
-    """Writes a chunk to the output delta lake table.
+def write_chunk_to_delta_table(batch: pa.Table, index: int, output_path: str) -> None:
+    """Writes a batch to the output delta lake table.
 
     Args:
-        chunk (pa.RecordBatch): The chunk to write
-        index (int): The position of the chunk, to control `append` or `overwrite` mode
+        batch (pa.Table): The batch to write
+        index (int): The position of the batch, to control `append` or `overwrite` mode
         output_path (str): The output delta table path
     """
 
     mode = "overwrite" if index == 0 else "append"
 
-    write_deltalake(table_or_uri=output_path, data=chunk, mode=mode, schema_mode="overwrite")
+    write_deltalake(table_or_uri=output_path, data=batch, mode=mode)
 
 
 def process(input_path: str, output_path: str, batch_size: int) -> pa.RecordBatch:
@@ -24,32 +26,34 @@ def process(input_path: str, output_path: str, batch_size: int) -> pa.RecordBatc
         input_path (str): Path/to the delta table
         output_path (str): Path/to the output delta table
         batch_size (int): The size of the chunks
-
-    Yields:
-        pa.RecordBatch: The processed chunk
     """
 
     dt = DeltaTable(table_uri=input_path, storage_options={"timeout": "3600s"})
 
     pa_dataset = dt.to_pyarrow_dataset()
 
-    for i, chunk in enumerate(pa_dataset.to_batches(batch_size=batch_size)):
+    for i, batch in enumerate(pa_dataset.to_batches(batch_size=batch_size)):
         logger.info(f"Processing chunk {i}...")
 
-        # Simulate a dummy processing
-        chunk_df = chunk.to_pandas()
-        chunk_df["avg_embeddings"] = chunk_df.apply(lambda row: row["vector"].mean(), axis=1)
-        processed_chunk = pa.RecordBatch.from_pandas(chunk_df)
+        batch_table = pa.Table.from_batches([batch])  # force read a batch
 
-        write_chunk_to_delta_table(
-            processed_chunk, i, output_path
+        # Simulate a dummy processing
+        time.sleep(3)  # takes 3 seconds!
+
+        write_chunk_to_delta_table(  # force write a batch
+            batch_table, i, output_path
         )  # commits new version for every chunk, but for this PoC is OK
+
+        logger.info(f"Processed chunk {i}")
 
 
 def main() -> None:  # noqa: D103
     input_path = (
         "s3://ml-rd-ml-datasets/generateVectorEmbed/Qwen3-Embedding-0.6B/miracl/fr/vector_corpus/"
     )
+
+    # TODO: How to establish the batch size?
+    # TODO: Process 1 parquet per iteration, instead of batch?
     batch_size = 25000  # ~100MB on disk
 
     output_path = "s3://mvp-mlops-platform/poc-multi-instance-data-prep-shards-batch-writes-delta/"
--- src/poc_multi_instance_data_preparation/sharded_by_key/seq_process.py
+++ src/poc_multi_instance_data_preparation/sharded_by_key/seq_process.py
@@ -1,13 +1,15 @@
 import os
+import time
 
-import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
 from loguru import logger
 
 INPUT_DIR = "/opt/ml/processing/input"
 OUTPUT_DIR = "/opt/ml/processing/output"
 
 
-def process(input_file: str) -> pd.DataFrame:
+def process(input_file: str) -> pa.Table:
     """Applies a dummy processing to the input file.
 
     Args:
@@ -17,23 +19,23 @@ def process(input_file: str) -> pd.DataFrame:
         pd.DataFrame: The resulting dataframe
     """
 
-    df = pd.read_parquet(input_file)
+    pa_table = pq.read_table(input_file)  # force reading shard
 
     # Simulate a dummy processing
-    df["avg_embeddings"] = df.apply(lambda row: row["vector"].mean(), axis=1)
+    time.sleep(3)  # takes 3 seconds!
 
-    return df
+    return pa_table
 
 
 def main() -> None:  # noqa: D103
-    # Process the shards copied to current instance sequentially
+    # Process the shards copied to current instance *sequentially*
     for i, file in enumerate(os.listdir(INPUT_DIR)):
         logger.info(f"Processing shard # {i}: {file}")
         file_path = os.path.join(INPUT_DIR, file)
-        df_out = process(file_path)
+        table_out = process(file_path)
 
         output_file_path = os.path.join(OUTPUT_DIR, f"processed_{file}")
-        df_out.to_parquet(output_file_path, index=False)
+        pq.write_table(table_out, output_file_path)  # force writing the shard
 
 
 if __name__ == "__main__":
