b91b2cc39d75fbb78dfe532d8f2dd0aacf362677
Ana Villalba <ana.villalba@elastic.co>
2025-07-30 15:56:36 +0200

    Implement a more realistic dummy processing in the sharding script

--- src/poc_multi_instance_data_preparation/sharded_by_key/seq_process.py
+++ src/poc_multi_instance_data_preparation/sharded_by_key/seq_process.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import time
 
@@ -5,8 +6,7 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 from loguru import logger
 
-INPUT_DIR = "/opt/ml/processing/input"
-OUTPUT_DIR = "/opt/ml/processing/output"
+from poc_multi_instance_data_preparation.utils import auth, log_memory
 
 
 def process(input_file: str) -> pa.Table:
@@ -22,20 +22,44 @@ def process(input_file: str) -> pa.Table:
     pa_table = pq.read_table(input_file)  # force reading shard
 
     # Simulate a dummy processing
-    time.sleep(3)  # takes 3 seconds!
+    vec_array = pa_table["vector"].to_pylist()
+    multiplied_array = [[x * 2 for x in sublist] for sublist in vec_array]
+    new_column = pa.array(multiplied_array, type=pa_table.schema.field("vector").type)
+    pa_table = pa_table.append_column("vector_x2", new_column)
 
     return pa_table
 
 
 def main() -> None:  # noqa: D103
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--local_mode", type=bool, required=False, default=False)
+    args = parser.parse_args()
+
+    if args.local_mode:
+        auth()
+        input_dir = "data"
+        output_dir = "outputs"
+    else:  # in Sagemaker, we mount these directories via ProcessingInputs and ProcessingOutputs
+        input_dir = "/opt/ml/processing/input"
+        output_dir = "/opt/ml/processing/output"
+
     # Process the shards copied to current instance *sequentially*
-    for i, file in enumerate(os.listdir(INPUT_DIR)):
+    for i, file in enumerate(os.listdir(input_dir)):
         logger.info(f"Processing shard # {i}: {file}")
-        file_path = os.path.join(INPUT_DIR, file)
+        log_memory()
+        start_time = time.time()
+
+        file_path = os.path.join(input_dir, file)
         table_out = process(file_path)
 
-        output_file_path = os.path.join(OUTPUT_DIR, f"processed_{file}")
-        pq.write_table(table_out, output_file_path)  # force writing the shard
+        logger.info(f"Processing applied to shard {i}")
+        log_memory()
+
+        output_file_path = os.path.join(output_dir, f"processed_{file}")
+        pq.write_table(table_out, output_file_path)
+
+        logger.info(f"Shard {i} saved to disk")
+        logger.info(f"Shard {i} processing time: {time.time() - start_time:.2f}s")
 
 
 if __name__ == "__main__":
