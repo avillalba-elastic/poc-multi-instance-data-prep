8715fd3230d93728cf5b84b3b0f97cd023a5de56
Ana Villalba <ana.villalba@elastic.co>
2025-08-08 12:23:01 +0200

    Add reading/writing directly to s3 with ray as a separate pipeline

--- deploy_pipeline.sh
+++ deploy_pipeline.sh
@@ -1,5 +1,8 @@
 aws stepfunctions create-state-machine --name poc-multi-instance-data-prep-seq-sharded-by-key --definition file://pipelines/seq_sharded_by_key.json --role-arn arn:aws:iam::879381254630:role/service-role/StepFunctions-mvp_mlops_platform_data_preprocessi-role-bn368ux1z --profile ml
 aws stepfunctions create-state-machine --name poc-multi-instance-data-prep-delta-batch-processing --definition file://pipelines/delta_batch_processing.json --role-arn arn:aws:iam::879381254630:role/service-role/StepFunctions-mvp_mlops_platform_data_preprocessi-role-bn368ux1z --profile ml
+
+aws stepfunctions create-state-machine --name poc-multi-instance-data-prep-ray-s3-sagemaker-processing --definition file://pipelines/ray_s3_sagemaker_processing.json --role-arn arn:aws:iam::879381254630:role/service-role/StepFunctions-mvp_mlops_platform_data_preprocessi-role-bn368ux1z --profile ml
 aws stepfunctions create-state-machine --name poc-multi-instance-data-prep-ray-sagemaker-processing --definition file://pipelines/ray_sagemaker_processing.json --role-arn arn:aws:iam::879381254630:role/service-role/StepFunctions-mvp_mlops_platform_data_preprocessi-role-bn368ux1z --profile ml
+
 
 #aws stepfunctions update-state-machine --state-machine-arn {arn} --definition file://pipelines/{pipeline_definition}.json --profile ml
