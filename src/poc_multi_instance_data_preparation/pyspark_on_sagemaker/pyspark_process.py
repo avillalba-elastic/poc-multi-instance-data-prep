ae9f3c615ed169cf681d97d5c9ec1b9681db2413
Ana Villalba <ana.villalba@elastic.co>
2025-08-12 17:04:31 +0200

    Adjust Spark JVM memory

--- src/poc_multi_instance_data_preparation/pyspark_on_sagemaker/pyspark_process.py
+++ src/poc_multi_instance_data_preparation/pyspark_on_sagemaker/pyspark_process.py
@@ -42,52 +42,56 @@ def main() -> None:  # noqa: D103
         # Point to the whole dataset at s3. You could also load this locally, after auth to AWS.
         # However you are going to run into OOO issues if your dataset does not fit in your local
         # resources.
+        # We use the Hadopp S3A protocol to access S3 from Spark
         INPUT_DELTA_TABLE_PATH = (
             "s3a://mvp-mlops-platform/poc-multi-instance-data-prep-repartitioned-delta/"
         )
         OUTPUT_DELTA_TABLE_PATH = f"s3a://mvp-mlops-platform/poc-multi-instance-data-prep-delta-pyspark_outputs/instance_count={args.n_instances}"
 
-    # See: https://docs.delta.io/latest/quick-start.html#set-up-apache-spark-with-delta-lake
-    # and https://docs.delta.io/latest/releases.html.
-    # Also see: https://aws.amazon.com/blogs/machine-learning/load-and-transform-data-from-delta-lake-using-amazon-sagemaker-studio-and-apache-spark/
-    # In the `pyspark` Docker container, we use Scala 2.12 and Spark 3.5.0:
+    # Dependencies for Spark. See: https://aws.amazon.com/blogs/machine-learning/load-and-transform-data-from-delta-lake-using-amazon-sagemaker-studio-and-apache-spark/
+    # Delta Lake <-> Spark compability matrix at: https://docs.delta.io/latest/releases.html.
+    # Delta Lake installation: https://docs.delta.io/latest/quick-start.html#set-up-apache-spark-with-delta-lake
+    # In Sagemaker, we use a prebuilt Docker image with Spark = 3.5.0 and Scala 2.12
+    # (see docker bake).
     packages = ",".join(
         [
+            # Delta Lake connector for Spark, compiled for Scala 2.12 and Delta Lake = 3.2.0,
+            # compatible with Spark 3.5.0
             "io.delta:delta-spark_2.12:3.2.0",
+            # Hadoop module to connect to S3 using S3A protocol. Compatible with Spark 3.5.0.
             "org.apache.hadoop:hadoop-aws:3.4.0",
+            # AWS SDK for Java, needed to use hadoop-aws
             "com.amazonaws:aws-java-sdk-bundle:1.12.329",
         ]
     )
 
     spark = (
         SparkSession.builder.appName("MyApp")
-        .config("spark.jars.packages", packages)
+        .config(
+            "spark.jars.packages", packages
+        )  # dependencies to download and add to JVM classpath
         .config(
             "fs.s3a.aws.credentials.provider", "com.amazonaws.auth.ContainerCredentialsProvider"
-        )
-        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        )  # to use Sagemaker IAM Role
         .config(
-            "spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog"
+            "spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension"
+        )  # to extend Spark SQL with Delta Lake functions
+        .config(
+            "spark.sql.catalog.spark_catalog",
+            "org.apache.spark.sql.delta.catalog.DeltaCatalog",  # Use Delta Lake as default catalog
         )
+        # The dataset is partitioned in Parquet files of ~1GB each. Thus, we need to give enough
+        # memory to the Spark JVM
+        .config("spark.executor.memory", "28g")
+        .config("spark.executor.memoryOverhead", "4g")
+        .config("spark.driver.memory", "28g")
         .getOrCreate()
     )
 
     logger.info(f"Reading Delta table from: {INPUT_DELTA_TABLE_PATH}")
     df = spark.read.format("delta").load(INPUT_DELTA_TABLE_PATH)
 
-    # To fairly compare Ray vs. Spark in terms of performance, we need to handle the
-    # same batch size. However, there are more aspects to take into account to choose
-    # between Spark or Ray.
-    total_rows = df.count()
-    batch_size = 220224 / 8  # same as Ray
-    num_partitions = int(max(1, total_rows // batch_size))
-    df = df.repartition(num_partitions)
-
-    logger.info(
-        f"Processing the dataset at: {INPUT_DELTA_TABLE_PATH};"
-        f"# of partitions = {num_partitions};"
-        f"batch size = {batch_size}"
-    )
+    logger.info(f"Processing the dataset at: {INPUT_DELTA_TABLE_PATH};")
     schema = StructType(
         [
             StructField("doc_id", StringType()),
